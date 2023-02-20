43afe5ced5c6d460d12fc5051558da9928e5aaec
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-02-20 15:36:55 +0500

    deploy_3

--- shop/admin.py
+++ shop/admin.py
@@ -17,16 +17,6 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
     # raw_id_fields = ('parent',)
 
-    # change_list_template = "csv_apload.html"
-
-    # def get_urls(self):
-    #     urls = super().get_urls()
-    #     my_urls = [
-    #         path('import-csv/', self.import_csv),
-    #     ]
-    #     return my_urls + urls
-
-
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
