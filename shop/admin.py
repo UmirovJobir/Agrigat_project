d9e68d13e5bb1a838d1da76ac0995c5e58867b7a
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-03-29 16:22:43 +0500

    create func in ProductSerializer

--- shop/admin.py
+++ shop/admin.py
@@ -29,7 +29,7 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
+    list_display = ('id', 'name', 'parent')
     search_fields = 'id', 'name',
     raw_id_fields = 'parent',
 
@@ -48,7 +48,7 @@ class CategoryAdmin(admin.ModelAdmin):
                 df = pd.read_excel(excel_file)
                 count = 0
                 for i in df.values:
-                    Category.objects.create(id=i[0], name=json.loads(i[1]), parent=i[2])
+                    Category.objects.create(id=i[0], name=json.loads(i[1]))
                     count += 1
                 messages.info(request,f"{count} data added!")
             else:
@@ -63,12 +63,17 @@ class CategoryAdmin(admin.ModelAdmin):
             if excel_file.name.endswith('.xlsx'):
                 df = pd.read_excel(excel_file)
                 count = 0
+                notfound = 0
                 for i in df.values:
-                    category = get_object_or_404(Category, id=i[1])
-                    category.parent = get_object_or_404(Category, id=i[0])
-                    category.save()
-                    count += 1
+                    try:
+                        category = Category.objects.get(id=i[1])
+                        category.parent = Category.objects.get(id=i[0])
+                        category.save()
+                        count += 1
+                    except Category.DoesNotExist:
+                        notfound += 1
                 messages.info(request,f"{count} data changed!")
+                messages.warning(request, f"{notfound} category not found!")
             else:
                 messages.error(request,"File is not in .xslx format!")
         form = ExcelImportForm()
--- shop/serializers.py
+++ shop/serializers.py
@@ -16,32 +16,6 @@ class ProductUserSerializer(serializers.ModelSerializer):
         model = ProductUser
         fields = 'id', 'user_id', 'user_name', 'user_link', 'phone_number'
 
-class ProductSerializer(serializers.ModelSerializer):
-    product_user = ProductUserSerializer()
-    category = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Product
-        fields = [
-            'id', 'product_user', 'category', 
-            'group_id', 'group_name', 'group_link',
-            'message_id', 'message_text', 'media_file', 
-            'datatime', 'status'
-        ]
-
-    # Custom ProductUser()
-    def create(self, validated_data):
-        product_user_data = validated_data.get("product_user")
-
-        product_user, created = ProductUser.objects.get_or_create(**product_user_data)
-        
-        product = Product.objects.create(product_user=product_user, **product_user_data)            
-
-        return product_user
-
-
-
-
 
 class CategorySerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField("category_name")
@@ -55,6 +29,30 @@ class CategorySerializer(serializers.ModelSerializer):
         name = category.name
         return name.get(lan)
 
+class ProductSerializer(serializers.ModelSerializer):
+    product_user = ProductUserSerializer()
+
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'product_user', 'category', 
+            'group_id', 'group_name', 'group_link',
+            'message_id', 'message_text', 'media_file', 
+            'datatime', 'status'
+        ]
+
+    def create(self, validated_data):
+        product_user_data = validated_data.pop("product_user")
+        category_data = validated_data.pop("category")
+
+        product_user, created = ProductUser.objects.get_or_create(**product_user_data)
+        
+        product = Product.objects.create(product_user=product_user, **validated_data)   
+
+        for category_d in category_data:
+            product.category.add(category_d)
+
+        return product
 
 class KeyWordsPostSerializer(serializers.ModelSerializer):
     class Meta:
