2c193b9f6492511852c7747573ec3522e2837a0a
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-05-04 16:57:13 +0500

    delete some func from chart

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
 from .views import (
     UserView,
     ProductUserView,
@@ -15,7 +14,7 @@ from .views import (
     CheckKeywordByWord,
     UpdateProductByGroupId
 )
-from util.chart import get_groups, get_months, get_days, get_all_products_in_a_day, get_products_len_in_a_day_by_groups, home
+from util.chart import get_months, get_days, get_products_len_in_a_day_by_groups
 
 
 urlpatterns = [
@@ -36,12 +35,8 @@ urlpatterns = [
 
     path('update_by_group_id/', UpdateProductByGroupId.as_view()),
     
-    path("get_groups/", get_groups, name="get-groups"),
     path("get_months/", get_months, name="get-months"),
     path("get_days/<int:month>", get_days, name="get-days"),
-    path("get_products/<int:month>/<int:day>", get_all_products_in_a_day, name="get-products"),
     path("get_groups_products/<int:month>/<int:day>/", get_products_len_in_a_day_by_groups, name="get-groups-products"),
-
-    path("home/", home, name='home')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
--- util/chart.py
+++ util/chart.py
@@ -1,34 +1,10 @@
 from django.db.models.functions import ExtractYear, ExtractMonth, ExtractDay
 from django.contrib.admin.views.decorators import staff_member_required
+from urllib.parse import urlencode
 from django.shortcuts import render
 from django.http import JsonResponse
 from shop.models import Product, Group, User
-from shop.serializers import GroupSerializer
-from django.db.models import Count, F, Sum, Avg
-from datetime import datetime
-import pandas as pd
-import pytz
 import json
-from urllib.parse import urlencode
-
-months = [
-    "January", "February", "March", "April",
-    "May", "June", "July", "August",
-    "September", "October", "November", "December"
-]
-
-colorPalette = ["#55efc4", "#81ecec", "#a29bfe", "#ffeaa7", "#fab1a0", "#ff7675", "#fd79a8"]
-colorPrimary, colorSuccess, colorDanger = "#79aec8", colorPalette[0], colorPalette[5]
-
-def get_year_dict():
-    year_dict = dict()
-
-    for month in months:
-        year_dict[month] = 0
-
-    
-    return year_dict
-
 
 
 # @staff_member_required
@@ -41,15 +17,7 @@ def get_year_dict():
 #     })
 
 
-def get_groups(request):
-    groups = Group.objects.all()
-    group_ids = [group.id for group in groups]
-
-    return JsonResponse({
-        "months": group_ids,
-    })
-
-
+@staff_member_required
 def get_months(request):
     products = Product.objects.annotate(month=ExtractMonth("datetime")).values("month").order_by("month").distinct()
     months = [product["month"] for product in products]
@@ -58,7 +26,7 @@ def get_months(request):
         "months": months,
         },)
 
-
+@staff_member_required
 def get_days(request, month):
     products = Product.objects.filter(datetime__month=month)
     days_group = products.annotate(day=ExtractDay("datetime")).values("day").order_by("day").distinct()
@@ -69,15 +37,7 @@ def get_days(request, month):
     })
     
 
-def get_all_products_in_a_day(request, month, day):
-    products = Product.objects.filter(datetime__month=month, datetime__day=day).distinct()
-    product_ids = [f"{product.id}    {product.datetime}" for product in products]
-
-    return JsonResponse({
-        'len':len(product_ids),
-        "product_id": product_ids,
-    })
-
+@staff_member_required
 def get_products_len_in_a_day_by_groups(request, month, day):
     products = Product.objects.filter(datetime__month=month, datetime__day=day)
     groups = Group.objects.filter(group_test__in=products).distinct()
@@ -94,37 +54,3 @@ def get_products_len_in_a_day_by_groups(request, month, day):
     })
 
 
-
-
-
-
-
-def home(request):
-    users = User.objects.all()
-    user_names = [user.user_name for user in users]
-    user_ids = [user.user_id for user in users]
-
-    config = {
-        "type": "bar",
-        "data": {
-            "labels": user_names,
-            "datasets": [{
-                "label": "Foo",
-                "data": user_ids
-            }]
-        }
-    }
-
-    params = {
-        'chart': json.dumps(config),
-        'width': 500,
-        'height': 300,
-        'backgroundColor': 'white',
-    }
-    print('https://quickchart.io/chart?%s' % urlencode(params))
-
-    my_dict = {
-        'link':'https://quickchart.io/chart?%s' % urlencode(params),
-    }
-
-    return render(request, 'admin/test_chart.html',context=my_dict)
